a81e83ee9989f7ebeeed3a1574592def81d0f45c
franciuine <74317191+franciuine@users.noreply.github.com>
2021-03-26 13:25:51 -0300

    Update Candidato.java

--- Trabalho 1/Candidato.java
+++ Trabalho 1/Candidato.java
@@ -1,29 +1,78 @@
-public class Candidato {
-	
-	public int votos = 0;
-	public String nome;
-	private String partido;
+import java.util.Scanner;
+
+public class Main{
+	public static void main (String[] args){
 		
-	public Candidato(String nome, String partido){
-		this.nome=nome;
-		this.partido=partido;
+		Scanner input = new Scanner(System.in);
+		Candidato listaCandidatos[] = new Candidato[3];
+		listaCandidatos[0] = new Candidato("Angela Pepino", "PMDB");
+		listaCandidatos[1] = new Candidato("Gean da Silva", "PT");
+		listaCandidatos[2] = new Candidato("Cesar Souza Neto", "DEM");
+		int votos = 0, brancos = 0, nulos = 0;
+		int maisVotos = 0;
+		int op = 0;
+		String eleito = null;
+		
+		while(op!=2){
+			System.out.println("\n|----Eleições Dev2Me----|");
+			System.out.println("| 1 - Inserir voto      |");
+			System.out.print("| 2 - Encerrar votação  |\n| ");
+			op = input.nextInt();
+			
+			switch(op){
+				case 1:
+					votos++;
+					System.out.println("\n\n| 1 - Angela Pepino       |");
+					System.out.println("| 2 - Gean da Silva       |");
+					System.out.println("| 3 - Cesar de Souza Neto |");
+					System.out.println("| 4 - Voto nulo           |");
+					System.out.print("| 5 - Voto em branco      |\n| ");
+					
+					switch(input.nextInt()){
+						case 1:
+							listaCandidatos[0].votos++;
+							break;
+						case 2:
+							listaCandidatos[1].votos++;
+							break;
+						case 3:
+							listaCandidatos[2].votos++;
+							break;
+						case 4:
+							nulos++;
+							break;
+						case 5:
+							brancos++;
+							break;
+						default:
+							System.err.println("\nX OPÇÃO INVÁLIDA! X");
+					        break;
+					}
+				
+					break;
+				case 2:
+					for(int i=0; i<3; i++){
+						listaCandidatos[i].Imprimir(i);
+						if(listaCandidatos[i].votos > maisVotos){
+							eleito = listaCandidatos[i].nome;
+							maisVotos = listaCandidatos[i].votos;
+						}
+					}
+					
+					if (votos == 0){
+						System.out.println("\n| Não houveram votos o suficiente para concluir as eleições.");
+					} else {
+						System.out.println("\n| " + eleito + " foi eleito(a) com " + maisVotos + " votos");
+						System.out.println("| Total de votos: " + votos);	
+						System.out.println("| Total de votos nulos: " + nulos);
+						System.out.println("| Total de votos em branco: " + brancos);
+			     	        }
+					System.exit(1);
+					break;
+				default:
+					System.err.println("\nX OPÇÃO INVÁLIDA! X");
+					break;
+			}
+		}	
 	}
-	
-	
-	/*public void setNome(String nome){
-		this.nome=nome;
-	}
-	
-	public void setPartido(String partido){
-		this.partido=partido;
-	}*/
-	
-	
-	public void Imprimir(int i){
-		System.out.println("\n| Código: " + (i+1));
-		System.out.println("| Candidato: " + this.nome);
-		System.out.println("| Votos: " + this.votos);
-		System.out.println("| Partido: " + this.partido);
-	}
-	
 }
