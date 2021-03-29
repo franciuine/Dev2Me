cd9692d99de544fa433ba025cf3cbcb4bcab0722
franciuine <74317191+franciuine@users.noreply.github.com>
2021-03-29 16:41:54 -0300

    Update Main.java

--- Exercícios/Aula 2/Exercício 2/Main.java
+++ Exercícios/Aula 2/Exercício 2/Main.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class Main{
+	public static void main (String[] args){
+		Scanner input = new Scanner(System.in);
+		int n = 0, verifica = 0;
+		
+		System.out.print("| Número: \n| ");
+		n = input.nextInt();
+		verifica = Verificar(n);
+		
+		if(verifica == 1){
+			System.out.println("| " + n + " é um número positivo.");
+		}
+		else if(verifica == -1){
+			System.out.println("| " + n + " é um número negativo.");
+		}
+		
+	}
+	
+	public static int Verificar(int numero){
+		if(numero > 0){
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+}
